77439daa9fc5db5b8aed70247e13f6d29a61f360
Nicolas Hernandez Vega <nihernandezv@udistrital.edu.co>
2025-09-03 19:40:27 -0500

    Expelliarmus.java

--- juego/hechizos/Expelliarmus.java
+++ juego/hechizos/Expelliarmus.java
@@ -7,14 +7,4 @@ public class Expelliarmus implements Hechizo {
     }
 }
 
-// -Snape:Lo criaste como un cerdo para el matadero
 
--Dumbledore:Porfavor Severus no me vas a decir que ahora le tomaste cariño
-
--Snape:Expecto Patronum
-
--Dumbledore:ah Lily! ,¿Despues de todo este tiempo?
-
--Snape:Siempre
-
-:'v
